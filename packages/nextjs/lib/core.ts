8a57fbb710b12c527d484d937af0aaf3f3876dd3
Kenny Rch <ken@99nth.com>
2025-11-10 13:06:13 +0300

    fix: Use Pinata gateway for IPFS uploads.

--- packages/nextjs/lib/core.ts
+++ packages/nextjs/lib/core.ts
@@ -1,6 +1,16 @@
 import { create } from 'kubo-rpc-client';
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const ipfs = create({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' }); // Use Infura for demo; switch to local node if running kubo locally
+const ipfs = create({
+  host: process.env.PINATA_GATEWAY,
+  port: 443,
+  protocol: 'https',
+  headers: {
+    authorization: `Bearer ${process.env.PINATA_JWT}`,
+  }
+  
+});
 
 export async function uploadToIPFS(data: object): Promise<string> {
   const result = await ipfs.add(JSON.stringify(data));

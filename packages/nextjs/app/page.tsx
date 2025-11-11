3eb5689f25fb9324ddd6fd44478d8ed6822f73ec
Kenny Rch <ken@99nth.com>
2025-11-11 15:26:38 +0300

    fix: update import path for uploadToIPFS function.

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -5,7 +5,7 @@ import type { NextPage } from "next";
 import toast from "react-hot-toast";
 import { useAccount } from "wagmi";
 import { useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
-import { uploadToIPFS } from "~~/lib/core";
+import { uploadToIPFS } from "~~/lib/utils";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
